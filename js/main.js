6d26c25f3f944b3f6be4f46c1f6f13bbfb5ba947
LRgonzalezB <luisrenegonzalezpe@gmail.com>
2025-03-06 20:02:55 -0500

    add page exercises

--- js/main.js
+++ js/main.js
@@ -1,73 +1,4 @@
 
-// // const imc = (a, b) => a / (b ** 2);
-// const proteinPerDay = (a, b) => a * b;
-// const carbPerDay = (a, b) => a * b;
-
-// let protein = 1.6;
-// let carbohydrates = 1;
-
-
-// // funcion para las preguntas
-
-
-//     function requestNumber(message) {
-//         let num;
-//         do {
-//             num = parseFloat(prompt(message));
-//             if (isNaN(num) || num < 0) {
-//                 alert("Por favor, ingrese un valor válido.");
-//             }
-//         } while (isNaN(num) || num < 0);
-//         return num;
-//     }
-
-
-// // funcion que calcula el imc
-
-// function calcImc() {
-//     let weight = requestNumber("ingrese su peso en kg")
-//     let size = requestNumber("ingrese su tamaño en metros")
-//     let resultImc = imc(weight, size)
-
-//     alert(`tu IMC es: ${resultImc.toFixed(2)}`)
-
-
-
-// function recommendNutricionalPlan(weight) {
-//    
-//     ];
-//     let opcion = parseInt(prompt(
-//         "Elige tu objetivo:\n1- Aumentar masa muscular\n2- Mantenerte saludable\n3- Perder peso\n4- Perder peso con ejercicio"));
-
-//         if (opcion >= 1 && opcion <= 4) {
-
-//         let plan = planesNutricionales[opcion - 1]
-//         alert(`${plan.objetivo} debes consumir ${proteinPerDay(weight, plan.proteina).toFixed(2)}g de proteina` + `y ${carbPerDay(weight, plan.carbohidratos).toFixed(2)}g de carbohidratospor dia.`)
-//     } else {
-//         alert("opcion no valida")
-//     }
-// }
-
-// do {
-// let opening = prompt("te puedo ayudar con el calculo de tu imc (si/no)").toLowerCase()
-
-//     if (opening !== "si"){
-//         alert("gracias vuelva pronto")
-//         break
-//     }
-
-//     let {weight} = calcImc()
-
-//     let decicion = prompt("te gustaria recibir recomendaciones de alimentacion? (si/no)").toLowerCase()
-
-//     if (decicion === "si") {
-//         recommendNutricionalPlan(weight)
-//     }else{
-//         alert("gracias vuelva pronto")
-//     }
-
-// } while (true);
-
 const planesNutricionales = [
     { objetivo: "aumentar masa muscular", proteina: 2, carbohidratos: 4, min_imc:0 , max_imc:18, message: "Tu peso es normal, ¡sigue así!" }, 
     { objetivo: "mantenerse saludable", proteina: 1.6, carbohidratos: 2.5, min_imc:19 , max_imc:24, message: "Tu peso es normal, ¡sigue así!" },
@@ -90,7 +21,7 @@ calcular_imc.onclick = () => {
     let carb_per_day = weight.value * buscarNumero[0].carbohidratos
 
     let print = document.createElement("h3")
-    print.innerHTML = `tu indice de masa cooporal es ${imc.toFixed(2)}`;
+    print.innerHTML = `tu indice de masa corporal es ${imc.toFixed(2)}`;
     
 
     let message = buscarNumero[0].message;
@@ -99,14 +30,14 @@ calcular_imc.onclick = () => {
     resultImc.innerHTML = "";
 
     let messageResultImc = document.createElement("div");
-    messageResultImc.innerHTML = `<h3> tu indice de masa cooporal es ${imc.toFixed(2)}</h3>
+    messageResultImc.innerHTML = `<h3> tu indice de masa corporal es ${imc.toFixed(2)}</h3>
     <p>${message}</p>
     <p>Tu consumo de proteina util por dia es ${protein_per_day}g</p>
     <p>Tu consumo de carbohidratos por dia es ${carb_per_day}g</p>
     `
-    
     resultImc.appendChild(messageResultImc)
 }
 
 
-   
+
+  
